da11dbfc5656a03774f191d101e420cb72d51018
Grigoriy Pryamov <pryamov.g@gmail.com>
2018-07-29 15:44:34 +0300

    Подготовка проекта
    Создание рабочей ветки + добавление моделя логирования

---
+++ logger/src/main/java/com/photoprint/logger/LoggerFactory.java
@@ -0,0 +1,16 @@
+package com.photoprint.logger;
+
+/**
+ *
+ * @author Grigoriy Pryamov.
+ */
+public class LoggerFactory {
+
+    public static Logger getLogger(String name) {
+        return new LoggerImpl(name);
+    }
+
+    public static Logger getLogger(Class<?> clazz) {
+        return getLogger(clazz.getSimpleName());
+    }
+}
