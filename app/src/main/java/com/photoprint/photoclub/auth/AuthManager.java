28dfc4096111760e8ec0b216e8dad1507257001e
Grigoriy Pryamov <gpryamov@digipeople.ru>
2018-08-21 09:24:01 +0300

    #Data
    - Обработка ошибки при получении токена

--- app/src/main/java/com/photoprint/photoclub/auth/AuthManager.java
+++ app/src/main/java/com/photoprint/photoclub/auth/AuthManager.java
@@ -4,6 +4,7 @@ import com.photoprint.logger.Logger;
 import com.photoprint.logger.LoggerFactory;
 import com.photoprint.network.Response;
 import com.photoprint.network.auth.AuthApiWorker;
+import com.photoprint.network.auth.model.login.DataToken;
 import com.photoprint.network.auth.model.login.DeviceToken;
 import com.photoprint.photoclub.data.authtokenstorage.AuthTokenStorage;
 import com.photoprint.photoclub.helper.runtimepermission.AppSchedulers;
@@ -40,15 +41,17 @@ public class AuthManager {
             return authApiWorker
                     .register()
                     .map(Response::getData)
+                    .onErrorReturn(throwable -> new DataToken())
                     .flatMapCompletable(dataToken ->
                             Completable.fromAction(() -> {
-                                        DeviceToken deviceToken = dataToken.getDeviceToken();
                                         String token;
+                                        DeviceToken deviceToken = dataToken.getDeviceToken();
                                         if (deviceToken != null) {
                                             token = deviceToken.getToken();
                                             logger.trace("successful register");
                                         } else {
                                             token = null;
+                                            logger.trace("failed register");
                                         }
                                         authTokenStorage.save(token);
                                     }
@@ -58,6 +61,5 @@ public class AuthManager {
             logger.trace("primary registration is not required");
             return Completable.complete();
         }
-
     }
 }
