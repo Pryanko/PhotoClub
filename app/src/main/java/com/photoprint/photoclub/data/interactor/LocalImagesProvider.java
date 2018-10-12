6e23ae12bfccdef85f97fa721c7faeb057a48360
Greg Pryamov <pryamov.g@gmail.com>
2018-10-12 09:03:05 +0300

    #LocalImagesProvider получение folder image файла

--- app/src/main/java/com/photoprint/photoclub/data/interactor/LocalImagesProvider.java
+++ app/src/main/java/com/photoprint/photoclub/data/interactor/LocalImagesProvider.java
@@ -6,12 +6,14 @@ import com.photoprint.photoclub.data.storage.StorageManager;
 import com.photoprint.photoclub.model.LocalImage;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import io.reactivex.Single;
+import io.reactivex.annotations.NonNull;
 
 /**
  * Поставщик фотографий устройства
@@ -24,6 +26,7 @@ public class LocalImagesProvider {
     private static final Logger logger = LoggerFactory.getLogger(LocalImagesProvider.class);
 
     private final static Integer DEFAULT_COUNT_PRINT_IMAGE = 1;
+    private final static String DEFAULT_NAME_SYSTEM_PATH = "Pictures";
 
     private final StorageManager storageManager;
 
@@ -39,10 +42,27 @@ public class LocalImagesProvider {
             for (String path : imagePaths) {
                 LocalImage localImage = new LocalImage();
                 localImage.setFullPath(path);
+                String folder = getFolderImage(path);
+                localImage.setParentFolder(folder);
+                logger.trace(folder);
                 localImage.setCountPrint(DEFAULT_COUNT_PRINT_IMAGE);
                 localImages.add(localImage);
             }
             return localImages;
         });
+    }
+
+    @NonNull
+    private String getFolderImage(String path) {
+        List<String> folderList = Arrays.asList(path.split("/"));
+        String folder = folderList.get(6);
+        if (folder.equals(DEFAULT_NAME_SYSTEM_PATH)) {
+            folder = folderList.get(7);
+        }
+        if (folder != null) {
+            return folder;
+        } else {
+            return DEFAULT_NAME_SYSTEM_PATH;
+        }
     }
 }
